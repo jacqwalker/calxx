ed03273d94b3d3e2bd68cdca1f4e706df1773834
jacqwalker <blackjack.walker@gmail.com>
2017-09-24 22:44:10 +0100

    JS initial commit

---
+++ js/index.js
@@ -0,0 +1,86 @@
+// cookies popup
+$(function () {
+  if(localStorage.getItem('popState') != 'shown'){
+      $(".cookies-popup-section").delay(2000).fadeIn();
+      localStorage.setItem('popState','shown');
+  } else {
+    document.querySelector(".cookies-popup-section").style.display = "none";
+  }
+  });
+
+$('.close-cookies-popup').click(function(e) {
+    $('.cookies-popup-section').fadeOut();
+  });
+
+// set up page loading job search
+$(function () {
+ $("#findJobs").val("");
+});
+
+// Nav bar
+function showNavBar(e) {
+  e.preventDefault();
+  $(".nav-header").toggleClass("visible");
+}
+
+$(".nav-icon").click(showNavBar);
+
+// Show and hide application form
+function showForm(e) {
+  e.preventDefault();
+  $(".application-section").addClass("application-section-visible");
+}
+
+$(".apply-now-button").click(showForm);
+
+function hideForm(e) {
+  e.preventDefault();
+  $(".application-section").removeClass("application-section-visible");
+}
+
+$(".close-apply-now-form").click(hideForm);
+
+// Show and hide job alert section
+function showJobAlerts(e) {
+  e.preventDefault();
+  $(".job-alert-section").addClass("job-alert-section-visible");
+}
+
+$(".job-alerts").click(showJobAlerts);
+
+
+function hideJobAlert(e) {
+  e.preventDefault();
+  $(".job-alert-section").removeClass("job-alert-section-visible");
+}
+
+$(".close-job-alerts").click(hideJobAlert);
+
+// Google maps on job spec page
+function initMap() {
+  var uluru = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+  zoom: 4,
+  center: uluru
+  });
+  var marker = new google.maps.Marker({
+  position: uluru,
+  map: map
+  });
+}
+
+// Linking find jobs to search page
+var newUrl;
+$("#findJobs").change(function () {
+  // newUrl = $("#findJobs").val();
+  newUrl = document.getElementById("findJobs").value;
+  }
+);
+
+$("#findJobsButton").click(function (e) {
+  e.preventDefault();
+  location = newUrl;
+  }
+);
+
+// Hide cookies button
